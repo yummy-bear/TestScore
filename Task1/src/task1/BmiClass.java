e8c4a062968ccde277bdc4251e8f6bb101dc90f3
yummy-bear <mudskipper7ki@gmail.com>
2021-02-04 22:30:33 +0900

    BMIの数値判別

---
+++ Task1/src/task1/BmiClass.java
@@ -0,0 +1,20 @@
+package task1;
+
+public class BmiClass {
+
+	public static void main(String[] args) {
+		double height = 1.60;
+		double weight = 70.9;
+		double bmi = weight / (height * height);
+		
+		if (bmi < 18.5) {
+			System.out.println("低体重");
+		} else if (bmi < 24.9) {
+			System.out.println("標準");
+		} else {
+			System.out.println("肥満");
+		}
+
+	}
+
+}
