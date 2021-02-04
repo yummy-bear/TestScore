1bdbc4a8638f9a0194d673c67e464f7c0dfd74e7
yummy-bear <mudskipper7ki@gmail.com>
2021-02-05 00:00:44 +0900

    2の二乗

---
+++ Task1/src/task3/TwoClass.java
@@ -0,0 +1,15 @@
+package task3;
+
+public class TwoClass {
+
+	public static void main(String[] args) {
+		int num1 = 2;
+		int i = 1;
+		int sum = 1;
+		while (i <= 8) {
+			System.out.println(sum *= num1);
+			i++;
+		}
+	}
+
+}
