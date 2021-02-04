dddcec93f9b11a377340eb27aac27a8320aeb096
yummy-bear <mudskipper7ki@gmail.com>
2021-02-04 23:15:40 +0900

    7の段

---
+++ Task1/src/task2/SevenClass.java
@@ -0,0 +1,13 @@
+package task2;
+
+public class SevenClass {
+
+	public static void main(String[] args) {
+		int i = 1;
+		while (i <= 9) {
+		System.out.println("7×" + i + "=" + 7 * i);
+		i++;
+		}
+	}
+
+}
