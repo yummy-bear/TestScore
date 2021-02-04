087aae40f3b5e537c9c5a1d6fe0d97abf2b0f3a7
yummy-bear <mudskipper7ki@gmail.com>
2021-02-04 21:40:57 +0900

    コンソールで入力するバージョン

--- Task1/src/task1/TestScore.java
+++ Task1/src/task1/TestScore.java
@@ -1,10 +1,18 @@
 package task1;
 
+import java.util.Scanner;
+
 public class TestScore {
 
 	public static void main(String[] args) {
-		int japanese = 40;
-		int math = 30;
+		
+		System.out.println("国語の点数を入力: ");
+        Scanner sc1 = new Scanner(System.in);
+        int japanese = sc1.nextInt();
+        System.out.println("数学の点数を入力: ");
+        Scanner sc2 = new Scanner(System.in);
+        int math = sc2.nextInt();
+        
 		int sum = japanese + math;
 		
 		if (japanese < 40 || math < 40 || sum <100) {
