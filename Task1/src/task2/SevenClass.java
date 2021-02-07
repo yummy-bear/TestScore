c3930400d3930e75d14c0cac4f13715e44b827d9
yummy-bear <mudskipper7ki@gmail.com>
2021-02-08 06:29:59 +0900

    newbmi

--- Task1/src/task2/SevenClass.java
+++ Task1/src/task2/SevenClass.java
@@ -3,10 +3,14 @@ package task2;
 public class SevenClass {
 
 	public static void main(String[] args) {
-		int i = 1;
-		while (i <= 9) {
-		System.out.println("7×" + i + "=" + 7 * i);
-		i++;
+//		int i = 1;
+//		while (i <= 9) {
+//		System.out.println("7×" + i + "=" + 7 * i);
+//		i++;
+//		}
+		
+		for (int i = 1; i <= 9; i++) {
+			System.out.println("7×" + i + "=" + 7 * i);
 		}
 	}
 
